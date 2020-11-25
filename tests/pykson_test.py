84cb09cf4f8150262d158076622e80517f3e77c2
毕勤 <leavic@gmail.com>
2020-11-25 10:57:52 +0800

    add power_status check test

--- tests/pykson_test.py
+++ tests/pykson_test.py
@@ -2,6 +2,8 @@ from PyKsonLib.models import KTHS_415BS
 
 if __name__ == '__main__':
     p = KTHS_415BS("/dev/ttyUSB0")
+    if not p.power_status:
+        print("not power on")
     p.get_status()
     # p_list=p.list_all_pgm()
     p.load_pgm('-40.pgm')
